fbeb04b263c5760f4eb4c0490eceae0e25c9a72f
Wesley Cook <wesleycook74@gmail.com>
2018-03-12 18:34:40 -0400

    Added carousel template

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')) // redirect bootstrap JS
+  .use('/js', express.static(__dirname + '/node_modules/jquery/dist')) // redirect JS jQuery
+  .use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')) // redirect CSS bootstrap
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
